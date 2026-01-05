fd4a40a4cad15b1076dea7d25d45172a59bfb621
bezelexconsulting-code <bezelexconsulting@gmail.com>
2026-01-05 08:09:51 +0000

    fix(super-admin): Tailwind v4 + PostCSS config and CSS wiring

--- super-admin/src/main.tsx
+++ super-admin/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { SuperAdminProvider } from './contexts/SuperAdminContext';
 import { SettingsProvider } from './contexts/SettingsContext';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
